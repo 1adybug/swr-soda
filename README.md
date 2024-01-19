98b2ba9fd3a9cd2fd3669dbf287b1264bf436f3a
luzixu <lurongv@qq.com>
2024-01-19 17:56:04 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,23 @@
+# swr-soda
+
+[![NPM version](https://img.shields.io/npm/v/swr-soda.svg?style=flat)](https://npmjs.org/package/swr-soda)
+[![NPM downloads](http://img.shields.io/npm/dm/swr-soda.svg?style=flat)](https://npmjs.org/package/swr-soda)
+
+## Install
+
+```bash
+$ yarn install
+```
+
+```bash
+$ npm run dev
+$ npm run build
+```
+
+## Options
+
+TODO
+
+## LICENSE
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import useSWR from "swr"
+import type { PublicConfiguration, Revalidator, RevalidatorOptions, SWRResponse } from "swr/_internal"
+export type { PublicConfiguration, Revalidator, RevalidatorOptions, SWRResponse } from "swr/_internal"
+
+export type FetcherConfigWithoutArg<Data> = {
+    fecther: () => Promise<Data>
+}
+
+export type FetcherConfigWithArg<Data, Arg> = {
+    arg: Arg
+    fecther: (arg: Arg) => Promise<Data>
+}
+
+export function fetcherWithoutArg<Data>(config: FetcherConfigWithoutArg<Data>): Promise<Data> {
+    return config.fecther()
+}
+
+export function fetcherWithArg<Data, Arg>(config: FetcherConfigWithArg<Data, Arg>): Promise<Data> {
+    return config.fecther(config.arg)
+}
+
+export type FetcherWithoutArg<Data> = (arg: FetcherConfigWithoutArg<Data>) => Promise<Data>
+
+export type FetcherWithArg<Data, Arg> = (arg: FetcherConfigWithArg<Data, Arg>) => Promise<Data>
+
+export type UseSWRWithArgOptions<Data, Arg> = Omit<PublicConfiguration<Data, any>, "fetcher" | "onLoadingSlow" | "onSuccess" | "onError" | "onErrorRetry"> & {
+    fetcher?: FetcherWithArg<Data, Arg>
+    onLoadingSlow: (key: string, config: Readonly<UseSWRWithArgOptions<Data, Arg>>) => void
+    onSuccess: (data: Data, key: string, config: Readonly<UseSWRWithArgOptions<Data, Arg>>) => void
+    onError: (err: any, key: string, config: Readonly<UseSWRWithArgOptions<Data, Arg>>) => void
+    onErrorRetry: (err: any, key: string, config: Readonly<UseSWRWithArgOptions<Data, Arg>>, revalidate: Revalidator, revalidateOpts: Required<RevalidatorOptions>) => void
+}
+
+export type UseSWRWithoutArgOptions<Data> = Omit<PublicConfiguration<Data, any>, "fetcher" | "onLoadingSlow" | "onSuccess" | "onError" | "onErrorRetry"> & {
+    fetcher?: FetcherWithoutArg<Data>
+    onLoadingSlow: (key: string, config: Readonly<UseSWRWithoutArgOptions<Data>>) => void
+    onSuccess: (data: Data, key: string, config: Readonly<UseSWRWithoutArgOptions<Data>>) => void
+    onError: (err: any, key: string, config: Readonly<UseSWRWithoutArgOptions<Data>>) => void
+    onErrorRetry: (err: any, key: string, config: Readonly<UseSWRWithoutArgOptions<Data>>, revalidate: Revalidator, revalidateOpts: Required<RevalidatorOptions>) => void
+}
+
+export function useSoda<Data, Options extends UseSWRWithoutArgOptions<Data> | undefined = undefined>(fetcher: FetcherWithoutArg<Data>, options?: Options): SWRResponse<Data, any, Options>
+export function useSoda<Data, Arg, Options extends UseSWRWithArgOptions<Data, Arg> | undefined = undefined>(arg: Arg, fetcher: FetcherWithArg<Data, Arg>, options?: Options): SWRResponse<Data, any, Options>
+export function useSoda(argOrFecther: any, fetcherOrOprions?: any, options?: any) {
+    if (typeof argOrFecther === "function" && typeof fetcherOrOprions !== "function") {
+        return useSWR({ fetcher: argOrFecther }, fetcherWithoutArg, fetcherOrOprions)
+    }
+    return useSWR({ arg: argOrFecther, fetcher: fetcherOrOprions }, fetcherWithArg, options)
+}
+
+export default useSoda
